2ccca26a993315685bfc8fbadff3c300dcc0453d
Isaac <your.email@example.com>
2025-02-14 19:11:47 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci = [0, 1]
+    for _ in range(2, length):
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci[:length])
+
+
